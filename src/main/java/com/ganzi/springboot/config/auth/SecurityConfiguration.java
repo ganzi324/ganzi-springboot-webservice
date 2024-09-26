f54d148726426d659ac845d40b5d49337a4b5af9
ganzi324 <ganzi324@gmail.com>
2024-09-26 14:48:00 +0900

    feat: chapter5 Security 설정

---
+++ src/main/java/com/ganzi/springboot/config/auth/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.ganzi.springboot.config.auth;
+
+import com.ganzi.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    private final CustomOAuth2UserService customOauth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headersConfigurer ->
+                        headersConfigurer
+                                .frameOptions(
+                                        HeadersConfigurer.FrameOptionsConfig::disable
+                                    )
+                )
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                        .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
+                        .anyRequest().authenticated()
+                )
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/")
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOauth2UserService)))
+                .httpBasic(withDefaults());
+        return http.build();
+
+    }
+}
