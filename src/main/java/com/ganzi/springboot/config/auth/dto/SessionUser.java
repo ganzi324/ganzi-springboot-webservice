4204f387ad2a8b0c2bc9c67d90dabec10fab23f4
ganzi324 <ganzi324@gmail.com>
2024-10-01 15:50:04 +0900

    fix: SessionUser 직렬화

--- src/main/java/com/ganzi/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/ganzi/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.ganzi.springboot.config.auth.dto;
 import com.ganzi.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
